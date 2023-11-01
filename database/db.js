00209033a07b12498eaf33d0ba05929fc32ceeb3
ArnoldRamodike <riccoarnold23@gmail.com>
2023-11-02 01:30:30 +0200

    env add

--- database/db.js
+++ database/db.js
@@ -1,10 +1,15 @@
 const mysql = require("mysql2/promise");
+const dotevn = require("dotenv").config();
 
 const mysqlPool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "Arnold994.",
-    database: "test"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: true,
+        // ca: serverCa
+    }
 });
 
 module.exports = mysqlPool;
